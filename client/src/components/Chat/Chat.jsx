288036881bc14f79257f75c696d3b793f3053eb2
Dang Luong <lsd25897@gmail.com>
2020-06-16 16:44:46 +0700

    add users, still has bug

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,29 +1,55 @@
-import React, {useState, useEffect} from "react";
-import queryString from "query-string";
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
 import io from "socket.io-client";
-import "./Chat.css";
+
+import './Chat.css';
+
 let socket;
-const Chat = ({location})=>{
+
+const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const ENDPOINT = "localhost:5000";
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'localhost:5000';
 
-    useEffect(()=>{
-        const test = location.search.split("?");
-        const nameTest = test[1].split("=");
-        const roomTest = test[2].split("=");
-        console.log(nameTest[1]);
-        console.log(roomTest[1]);
-        setName(nameTest[1]);
-        setRoom(roomTest[1]);
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+    
         socket = io(ENDPOINT);
-        console.log(name);
-        console.log(room);
-        //exact to the receiving end
-        socket.emit("join", {name, room});
-    }, [location.search]);
+    
+        setRoom(room);
+        setName(name)
+    
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+                alert(error);
+            }
+        });
+    }, [ENDPOINT, location.search]);
+    useEffect(()=>{
+        //get message
+        socket.on("message", (message)=>{
+            console.log("message in useEffect", message);
+            setMessages([...messages, message]);
+        });
+    },[messages]);
+    //send message
+    const sendMessage=(event)=>{
+        event.preventDefault();
+        if(message){
+            socket.emit("sendMessage", message, ()=>setMessage(""));
+        }
+    }
+    console.log("message",message);
+    console.log("messages",messages);
     return (
-        <h1>Hello there! Chat</h1>
-    )
+        <div className="outerContainer">
+            <div className="container">
+                <input value={message} onChange={(event)=>setMessage(event.target.value)} onKeyPress={(event)=>event.key==="Enter"?sendMessage(event):null}/>
+            </div> 
+        </div>
+    );
 }
+
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,20 +2,37 @@ const express = require("express");
 const socketIO = require("socket.io");
 const http = require("http");
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require("./users");
+
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
 const app =express();
 const server = http.createServer(app);
 const io= socketIO(server);
+
 io.on("connection", (socket)=>{
-    console.log("A new connection");
-    socket.on("join", (data)=>{
-        console.log(data);
+    //emit: sending end, on: receiving end
+    socket.on("join", ({name,room}, callback)=>{
+        const {error, user} = addUser({id:socket.id, name, room});
+        if(error) return callback(error);
+
+        socket.emit("message", {user:"admin", text:`${user.name}, welcome to the room ${user.room}!`})
+        //send message to everyone in that room beside the user
+        socket.broadcast.to(user.room).emit("message", {user:"admin", text:`${user.name} has joined the room`});
+        socket.join(user.room);
+        callback();
     })
+    socket.on("sendMessage", (message, callback)=>{
+        const user = getUser(socket.id);
+        io.to(user.room).emit("message", {user:user.name, text:message});
+        callback();
+    });
+    
     socket.on("disconnect",()=>{
         console.log("User had left");
     });
-})
+});
+
 app.use(router);
 
 server.listen(PORT, ()=>{console.log(`Server has started on ${PORT}`)});
